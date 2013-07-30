bde00778bc83e74d13abc374febe94a019ece80f
Nisio Takuya <littlecub240@gmail.com>
2013-07-30 12:35:38 +0900

    CreateDatabase edit

--- sql_data/create_database.sql
+++ sql_data/create_database.sql
@@ -1,1 +1,43 @@
 CREATE DATABASE webbootcamp CHARACTER SET utf8;
+
+use webbootcamp ;
+
+CREATE TABLE `users` (  
+`id` int(10) NOT NULL AUTO_INCREMENT,  
+`username` varchar(255) NOT NULL,  
+`password` varchar(255) NOT NULL,
+`nickname` varchar(255) NOT NULL,  
+`created` datetime NOT NULL,  
+`modified` datetime NOT NULL,  
+PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tasks` (
+ `id` int(10) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) NOT NULL,
+ `user_id` int(10) NOT NULL,
+ `todo_id` int(10) NOT NULL,
+ `status` int(1) NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `todos` (
+ `id` int(10) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) NOT NULL,
+ `user_id` int(10) NOT NULL,
+ `project_id` int(10) NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ `status` int(1) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `projects` (
+`id` int(10) NOT NULL AUTO_INCREMENT,  
+`name` varchar(255) NOT NULL,  
+`user_id` int(10) NOT NULL,  
+`created` datetime NOT NULL,  
+`modified` datetime NOT NULL,  
+PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
--- sql_data/create_projects.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `projects` ( 
-`id` int(10) NOT NULL AUTO_INCREMENT,  
-`name` varchar(255) NOT NULL,  
-`user_id` int(10) NOT NULL,  
-`created` datetime NOT NULL,  
-`modified` datetime NOT NULL,  
-PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8
